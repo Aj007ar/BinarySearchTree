8907d9cba55e3b20ab1d7ebf8b77a0e5b3f5f603
Aj007ar <aj007.ar@gmail.com>
2022-08-29 15:50:48 +0530

    [Ajay] Added: UC1 for Binary Search Tree code

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree");
+            BSTOperations<int> Bst = new BSTOperations<int>(56);
+            Bst.Insert(30);
+            Bst.Insert(70);
+            Console.WriteLine("Display using inorder traversal:");
+            Bst.Display();
+            Console.WriteLine(" ");
         }
     }
 }
